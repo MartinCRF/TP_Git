828d8ddb0ad6e64125cb21796225d1f7c2a30042
CORSEL-FAYE Martin <martin.corsel.faye@gmail.com>
2025-11-07 16:15:00 +0100

    add cod script

--- TP_GitProjet/Assets/Script/Perso_Controller.cs
+++ TP_GitProjet/Assets/Script/Perso_Controller.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Perso_Controller : MonoBehaviour
 {
+    [SerializeField] float _hahah = 0.1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
